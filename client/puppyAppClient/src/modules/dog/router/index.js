918e3bc6a93a1cd18cb98a707c6f781b4f6d17c7
busster <jasonmbuss@gmail.com>
2021-03-05 22:46:08 -0600

    break fix

--- client/puppyAppClient/src/modules/dog/router/index.js
+++ client/puppyAppClient/src/modules/dog/router/index.js
@@ -1,16 +1,17 @@
-import React from 'react'
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import Timeline from 'modules/dog/components/timeline'
 import Profile from 'modules/dog/components/profile'
 
-const Tab = createMaterialTopTabNavigator()
+const Stack = createStackNavigator();
 
 export const DogRouter = (props) => {
   return (
-    <Tab.Navigator initialRouteName="DogTimeline" headerMode="none">
-      <Tab.Screen name="DogTimeline" component={Timeline} />
-      <Tab.Screen name="DogProfile" component={Profile} />
-    </Tab.Navigator>
+    <Stack.Navigator initialRouteName="DogTimeline" headerMode="none">
+      <Stack.Screen name="DogTimeline" component={Timeline} />
+      <Stack.Screen name="DogProfile" component={Profile} />
+    </Stack.Navigator>
   )
 }
